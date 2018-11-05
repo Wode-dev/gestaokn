1aa41f167f3599067d9b4bbe73028d658eb809ad
KingNet <knprovedor@gmail.com>
2018-11-05 17:50:34 -0300

    Conectando ao mikrotik e retornando objeto de conexão

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,8 @@
 class ApplicationController < ActionController::Base
+
+
+    def connect_mikrotik
+        return MTik::Connection.new(:host=>"177.136.34.190", :user=>"admin",:pass=>"ALCATRAZ")
+    end
+
 end
