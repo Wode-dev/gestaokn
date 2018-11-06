f20dde47b0b5faf1cf690b085de3ecf29031e56a
KingNet <knprovedor@gmail.com>
2018-11-06 18:13:21 -0300

    Tirando método de conexão com o mikrotik do controlador e passando pros models

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,4 @@
 class ApplicationController < ActionController::Base
 
 
-    def connect_mikrotik
-        return MTik::Connection.new(:host=>"177.136.34.190", :user=>"admin",:pass=>"ALCATRAZ")
-    end
-
 end
--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,3 +1,7 @@
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
+
+  def self.connect_mikrotik
+      return MTik::Connection.new(:host=>"177.136.34.190", :user=>"admin",:pass=>"ALCATRAZ")
+  end
 end
