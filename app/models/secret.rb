d6b714942a5333deed19e08c229991da07a759bb
KingNet <knprovedor@gmail.com>
2018-11-06 18:08:43 -0300

    Alterar enabled no mikrotik ao mudar banco de dados

--- app/models/secret.rb
+++ app/models/secret.rb
@@ -1,3 +1,17 @@
 class Secret < ApplicationRecord
     belongs_to :plan
+
+    before_update :on_enabled_change
+
+    def on_enabled_change
+
+        if enabled_changed?
+            mk = MTik::Connection.new(:host=>"177.136.34.190", :user=>"admin",:pass=>"ALCATRAZ")
+            
+            puts mk.get_reply("/ppp/secret/set",
+                "=disabled=#{self.enabled ? "no" : "yes"}",
+                "=.id=#{mk.get_reply("/ppp/secret/print", 
+                "?name=#{self.secret}")[0][".id"]}")
+        end
+    end
 end
