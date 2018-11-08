299575183f38d9244f47496912456cbff0426c1c
KingNet <knprovedor@gmail.com>
2018-11-08 14:58:12 -0300

    Página main no inicial

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,6 @@ Rails.application.routes.draw do
 
   resources :secrets
   post "secrets/pay", to: "secrets#payment"
-  root to: "secrets#index"
+  root to: "main#home"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
