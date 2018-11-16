1a0b88ce867aae04a09637cdb168423cafcfc942
KingNet <knprovedor@gmail.com>
2018-11-16 11:32:38 -0300

    Adicionando o id do mikrotik ao sincronizar

--- app/controllers/sync_controller.rb
+++ app/controllers/sync_controller.rb
@@ -5,11 +5,15 @@ class SyncController < ApplicationController
     # Profile
     # Secret
     def sync_partial
-        mk = connect_mikrotik
+        mk = ApplicationRecord.connect_mikrotik
 
         all_plans = mk.get_reply("/ppp/profile/print")
         all_plans.each do |plan|
-          Plan.where(profile_name: plan["name"]).length > 0 ? nil : Plan.create(profile_name: plan["name"], rate_limit: plan["rate-limit"])
+          if Plan.where(profile_name: plan["name"]).length > 0 
+            Plan.create(profile_name: plan["name"], rate_limit: plan["rate-limit"])
+          else
+            Plan.where(profile_name: plan["name"]).update(mk_id: plan[".id"])
+          end
         end
     
         # TODO: adicionar o service e o remote-id no banco de dados quando atualizar
@@ -19,6 +23,8 @@ class SyncController < ApplicationController
                 Secret.create(secret: mk_secret["name"],
                 secret_password: mk_secret["password"],
                 plan_id: Plan.where(profile_name: mk_secret["profile"]).first.id )
+            else
+                Secret.where(secret: mk_secret["name"]).update(mk_id: mk_secret[".id"])
             end
         end
 
