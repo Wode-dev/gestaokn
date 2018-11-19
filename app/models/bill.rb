d21443c5fe79eeccee9495270f6c0f55ad52d7cd
KingNet <knprovedor@gmail.com>
2018-11-19 19:09:55 -0300

    Definindo atualização da situação do cliente sempre que houver pagamento ou consumo

--- app/models/bill.rb
+++ app/models/bill.rb
@@ -1,4 +1,8 @@
 class Bill < ApplicationRecord
     belongs_to :secret
 
+    after_update do
+
+        self.secret.balance
+    end
 end
--- app/models/payment.rb
+++ app/models/payment.rb
@@ -3,4 +3,9 @@ class Payment < ApplicationRecord
     belongs_to :secret
     belongs_to :payment_form
 
+    after_update do
+
+        self.secret.balance
+
+    end
 end
