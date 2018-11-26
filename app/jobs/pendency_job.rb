86b4edf2ae9e56ff417bac442d7fd61783300c32
KingNet <knprovedor@gmail.com>
2018-11-26 19:28:06 -0300

    Verificação de debito de pagamento e bloqueio automático

--- app/jobs/pendency_job.rb
+++ app/jobs/pendency_job.rb
@@ -1,7 +1,23 @@
 class PendencyJob < ApplicationJob
   queue_as :default
 
+  # Verifica quais usuários estão com pendência de pagamento, e atualiza o status
   def perform(*args)
-    # Do something later
+    
+    Secret.all.each do |secret|
+      secret.balance
+
+      in_debt = secret.situation < 0
+
+      # Caso o usuário não esteja registrado como atualização automática, o sistema não efetuará bloqueio automático
+      if secret.automatic_update
+        if in_debt
+          secret.update(enabled: false)
+        else
+          secret.update(enabled: true)
+        end
+      end
+    end
+
   end
 end
