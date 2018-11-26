419f79f39bf7d9ea83bcfa25b2deccf55a943a64
KingNet <knprovedor@gmail.com>
2018-11-26 17:06:24 -0300

    Programador de ação todo dia 5 do mês às 23 horas

--- config/initializers/scheduler.rb
+++ config/initializers/scheduler.rb
@@ -2,3 +2,13 @@ require 'rufus-scheduler'
 
 scheduler = Rufus::Scheduler.new
 
+# Teste
+scheduler.cron '*/5 * * * *' do
+    puts "Cron de 5 em 5 minutos executado"
+    #BillJob.perform_now
+end
+
+# Todo dia 5, às 23 horas no horário do nordeste
+scheduler.cron '0 23 5 * * America/Fortaleza' do
+    BillJob.perform_now
+end
