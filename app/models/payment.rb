bdcbec8e8d5a812f90a3b35d46687af2cdd6e5a0
KingNet <knprovedor@gmail.com>
2018-11-27 16:21:39 -0300

    Desbloquear cliente automaticamente após quitação dos débitos

--- app/models/payment.rb
+++ app/models/payment.rb
@@ -3,9 +3,16 @@ class Payment < ApplicationRecord
     belongs_to :secret
     belongs_to :payment_form
 
-    after_update do
+    after_update :verify_situation_to_enable
+    after_create :verify_situation_to_enable
 
+    def verify_situation_to_enable
         self.secret.balance
 
+        # Desbloquear se não tiver débito
+        @status = self.secret.situation_status
+        if self.secret.situation >= 0 || @status == "Regular"
+            self.secret.enabled_change true
+        end
     end
 end
