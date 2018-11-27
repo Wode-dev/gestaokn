1bb09aa2274eabb4b02c9d56cc2a8df65ac67812
KingNet <knprovedor@gmail.com>
2018-11-27 16:05:30 -0300

    Remoção do status "Vencimento hoje"

--- app/models/secret.rb
+++ app/models/secret.rb
@@ -61,10 +61,8 @@ class Secret < ApplicationRecord
         if self.bills.length > 0
 
           @due_date = self.bills.order(due_date: :desc).first.due_date
-          if @due_date > Date.today
+          if @due_date >= Date.today
             "Regular"
-          elsif @due_date == Date.today
-            "Vencimento hoje" 
           else
             puts @due_date - Date.today
             "Em Aberto"
