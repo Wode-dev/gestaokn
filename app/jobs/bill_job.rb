f65891efe49f841f7afd1eccbce7b376a5945bff
KingNet <knprovedor@gmail.com>
2018-11-28 19:01:17 -0300

    Job para fechar as contas usando o método dentro do secret

--- app/jobs/bill_job.rb
+++ app/jobs/bill_job.rb
@@ -5,25 +5,8 @@ class BillJob < ApplicationJob
   def perform(*args)
     
     # Caso o cliente esteja em estado de ativo, deverá fechar o faturamento do cliente
-    Secret.where(active: true).each do |sct|
-
-      # Ultima fatura do cliente
-      @bill = sct.bills.where(ref_end: nil).order(due_date: :desc)
-      if @bill.length > 0 && @bill.first.ref_end.nil?
-
-        #fecha fatura aberta
-        @value = ((Date.today - @bill.first.ref_start) / (Date.new(@bill.first.ref_start.year,@bill.first.ref_start.month + 1, 5) - Date.new(@bill.first.ref_start.year,@bill.first.ref_start.month, 6))) * sct.plan.value
-        @bill.first.update(ref_end: Date.today, value: @value)
-        
-        # cria nova fatura aberta
-        @next_month = 1.month.from_now
-        puts @next_month.year
-        puts @next_month.month
-        puts sct.due_date
-        @due_date = Date.new(@next_month.year.to_i, @next_month.month.to_i, sct.due_date.to_i)
-        sct.bills << Bill.new(ref_start: Date.today + 1, due_date: @due_date)
-      end
-    
+    Secret.where(active: true).each do |secret|
+      secret.close_last_open_bill
     end
   end
 end
