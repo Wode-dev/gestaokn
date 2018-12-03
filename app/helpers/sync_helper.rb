73b33edef3f953d7dedd56d147507ef7a5940ef0
KingNet <knprovedor@gmail.com>
2018-12-03 18:56:16 -0300

    Helper para exibir nome do record

--- app/helpers/sync_helper.rb
+++ app/helpers/sync_helper.rb
@@ -1,2 +1,14 @@
 module SyncHelper
+
+    def record_details(table, id)
+        case table
+        when "Secret"
+
+            Secret.find(id).secret
+        when "Plan"
+
+            Plan.find(id).profile_name
+        end
+    end
+    
 end
