0c0f9254be76cef245001de475bb6e8d071efa8b
KingNet <knprovedor@gmail.com>
2018-12-04 18:54:50 -0300

    Metodo de adicionar nota

--- app/controllers/secrets_controller.rb
+++ app/controllers/secrets_controller.rb
@@ -203,7 +203,18 @@ class SecretsController < ApplicationController
     redirect_to params[:fallback]
   end
 
+  def add_note
+    @data = params.permit(:id, :description)
 
+    Relationship.create(secret_id: @data[:id], description: @data[:description])
+
+    redirect_to secret_path(Secret.find(@data[:id]))
+  end
+
+  def edit_note
+    
+  end
+  
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_secret
