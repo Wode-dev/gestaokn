342408568488e9d05ac18882cd2ce6aac6eaf349
KingNet <knprovedor@gmail.com>
2018-12-04 16:07:45 -0300

    Acessando mikrotik por meio dos valores em configurações

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -2,6 +2,6 @@ class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
 
   def self.connect_mikrotik
-      return MTik::Connection.new(:host=>"192.168.25.250", :user=>"admin",:pass=>"ALCATRAZ")
+      return MTik::Connection.new(:host=>Setting.get(:mk_ip), :user=>Setting.get(:mk_user),:pass=>Setting.get(:mk_password))
   end
 end
