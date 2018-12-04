1701480cd583b19d311f957c73f3ed2767e87603
KingNet <knprovedor@gmail.com>
2018-12-04 14:49:15 -0300

    Adicionando configurações aos lugares certos

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -5,5 +5,14 @@ class MainController < ApplicationController
   def settings
     
   end
+
+  def save_settings
+    @settings = params.permit(:mk_ip, :mk_user, :mk_password)
+
+    Setting.insert(:mk_ip, @settings[:mk_ip])
+    Setting.insert(:mk_user, @settings[:mk_user])
+    Setting.insert(:mk_password, @settings[:mk_password])
+  end
+  
   
 end
