a859c059cf8280cb6318d6a546a05f29020a612a
KingNet <knprovedor@gmail.com>
2018-12-04 14:47:45 -0300

    Modelo de lidar com as configurações

--- app/models/setting.rb
+++ app/models/setting.rb
@@ -1,2 +1,23 @@
 class Setting < ApplicationRecord
+
+    def get(key)
+
+        @query = Setting.where(key: key)
+        if @query.length != 0
+            @query.first.value
+        else
+            nil
+        end
+    end
+
+    def insert(key, value)
+
+        @query = Setting.where(key: key)
+        if @query.length != 0
+            @query.first.update(value: value)
+        else
+            Setting.create(key: key, value: value)
+        end
+    end
+    
 end
