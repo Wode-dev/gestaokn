039e5dbed54983d4538731197e7c6963ce91d3e5
KingNet <knprovedor@gmail.com>
2018-12-06 16:46:06 -0300

    Método para confirmar a instalação como um novo cliente

--- app/controllers/records_controller.rb
+++ app/controllers/records_controller.rb
@@ -61,6 +61,10 @@ class RecordsController < ApplicationController
     end
   end
 
+  def confirm
+    
+  end
+
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_record
