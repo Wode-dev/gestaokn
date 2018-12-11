91f15b73cb82b2b70d204af80af2ee32ada32d61
KingNet <knprovedor@gmail.com>
2018-12-11 15:25:22 -0300

    Aceitar data de instalação no cliente

--- app/controllers/records_controller.rb
+++ app/controllers/records_controller.rb
@@ -86,6 +86,6 @@ class RecordsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def record_params
-      params.require(:record).permit(:name, :address, :city, :state, :neighborhood, :phone, :notes)
+      params.require(:record).permit(:name, :address, :city, :state, :neighborhood, :phone, :notes, :date)
     end
 end
