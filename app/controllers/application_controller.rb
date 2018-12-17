4c341b33853d9bcbfd3b14f5cb59e7a6460ef0e9
KingNet <knprovedor@gmail.com>
2018-12-17 13:33:46 -0300

    Retorno do método de tirar mascara de hash

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,8 @@ class ApplicationController < ActionController::Base
 
     def remove_mask_for_money(hash = {money: "100.000.000,00"}, key = "money")
         hash[key] = hash[key].gsub(".", "").gsub(",",".").to_f
+
+        hash
     end
 
 end
