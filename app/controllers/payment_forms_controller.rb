793bf93fb72e9b56e120548de127958aef47b3ad
KingNet <knprovedor@gmail.com>
2018-12-18 18:36:33 -0300

    Método de atualização

--- app/controllers/payment_forms_controller.rb
+++ app/controllers/payment_forms_controller.rb
@@ -3,6 +3,9 @@ class PaymentFormsController < ApplicationController
   end
 
   def update
+    PaymentForm.find(params[:id]).update(params.slice(:kind, :place))
+  
+    redirect_to payment_forms_path
   end
 
   def create
