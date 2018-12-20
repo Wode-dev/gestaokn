ee44381f938eb88620d33aa8876e1bfd6338d0dd
KingNet <knprovedor@gmail.com>
2018-12-20 15:57:15 -0300

    Transformando scheduler padrão em variável global

--- config/initializers/scheduler.rb
+++ config/initializers/scheduler.rb
@@ -1,18 +1,18 @@
 require 'rufus-scheduler'
 
-scheduler = Rufus::Scheduler.new
+$scheduler = Rufus::Scheduler.new
 
 # Teste
-scheduler.cron '*/5 * * * *' do
+$scheduler.cron '*/5 * * * *' do
     puts "Cron de 5 em 5 minutos executado"
     #BillJob.perform_now
 end
 
 # Todo dia 6, às 2 horas da manhã no horário do nordeste
-scheduler.cron '0 2 6 * * America/Fortaleza' do
+$scheduler.cron '0 2 6 * * America/Fortaleza' do
     BillJob.perform_now
 end
 
-scheduler.cron '0 0 * * * America/Fortaleza' do
+$scheduler.cron '0 0 * * * America/Fortaleza' do
     PendencyJob.perform_now
 end
