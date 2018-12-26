f5d4edb208d9d11bf157ac07579a3918c904c251
alberttoc <albertto.programmer@gmail.com>
2018-12-26 19:15:33 -0300

    Autenticar usuário antes de usar o sistema

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,7 @@
 class ApplicationController < ActionController::Base
 
+    before_action :authenticate_user!
+
     def remove_mask_for_money(hash = {money: "100.000.000,00"}, key = "money")
         hash[key] = hash[key].gsub(".", "").gsub(",",".").to_f
 
