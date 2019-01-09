3ca5f3e2cf95418e37925baefcfa7ec84ed1ef31
Alberto C <albertto.programmer@gmail.com>
2019-01-09 17:44:39 -0300

    Condições para levar para a tela de inicio

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,13 @@
 class ApplicationController < ActionController::Base
-
+    
     before_action :authenticate_user!
+
+    before_action do
+        if action_name != "initial_configuration" && Secret.count == 0
+            redirect_to initial_configuration_path
+        end
+    end
+
 
     def remove_mask_for_money(hash = {money: "100.000.000,00"}, key = "money")
         hash[key] = hash[key].gsub(".", "").gsub(",",".").to_f
